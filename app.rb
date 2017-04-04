48a056f1de05f1ea5c1aaf68dc99caf2821384ee
curtisgreene <curtis.j.w.greene@gmail.com>
2017-04-04 11:18:04 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,41 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+
+  get '/reversename/:name' do
+    "#{params[:name].reverse}"
+  end
+
+  get '/square/:number' do
+    num = params[:number].to_i
+    "#{num ** 2}"
+  end
+
+  get '/say/:number/:phrase' do
+    "#{params[:phrase]}\n" * params[:number].to_i
+  end
+
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+    "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
+  end
+
+  get '/:operation/:number1/:number2' do
+    if params[:operation] == "add"
+      sum = params[:number1].to_i + params[:number2].to_i
+      "#{sum}"
+    elsif params[:operation] == "subtract"
+      result = params[:number1].to_i - params[:number2].to_i
+      "#{result}"
+    elsif params[:operation] == "divide"
+      result = params[:number1].to_i / params[:number2].to_i
+      "#{result}"
+    elsif params[:operation] == "multiply"
+      result = params[:number1].to_i * params[:number2].to_i
+      "#{result}"
+    else
+      "Go home, you're drunk."
+    end
+  end
+
 end
